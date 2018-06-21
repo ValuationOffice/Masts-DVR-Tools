ec6e8947d564b86c65cffbd2e561631166683fc8
Jack Rhodes <jack-8729@hotmail.co.uk>
2018-06-21 14:12:16 +0100

    Move DI to UI layer to allow for injection into Views

--- masts_dvr_tool/App.xaml.cs
+++ masts_dvr_tool/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using masts_dvr_tool.DataAccess.Contracts;
+using masts_dvr_tool.DataAccess.Repository;
+using masts_dvr_tool.Services.Implementation;
+using Ninject;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +17,28 @@ namespace masts_dvr_tool
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ConfigureContainer();
+            ComposeObjects();
+
+        }
+
+        private IKernel Container { get; } = new StandardKernel();
+
+        private void ConfigureContainer()
+        {
+            //Bindings here
+            Container.Bind<IPDFRepository>().To<PDFRepository>().InSingletonScope();
+            Container.Bind<PDFManager>().To<PDFManager>().InSingletonScope();
+        }
+
+        private void ComposeObjects()
+        {
+            //View binding here
+
+        }
+
     }
 }
